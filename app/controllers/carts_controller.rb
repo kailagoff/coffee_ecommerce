e9b48788962ace96cbd9dc6929684a8ad66bb210
Kaila Goff and Maggie Summers <maggie@>
2018-05-02 14:59:59 -0700

    troubleshoot adding descriptions dynamically to page

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,9 @@
 class CartsController < ApplicationController
   def show
     @order_items = current_order.order_items
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { render 'carts/show.js.erb'}
+    end
   end
 end
