2edbcfd6c6b21e0faf309168945e57544c15546b
Kaila Goff and Maggie Summers <kaila@>
2018-05-03 08:44:05 -0700

    redirect root path

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -5,7 +5,7 @@ class OrderItemsController < ApplicationController
     @item = @order.order_items.new(item_params)
     @order.save
     session[:order_id] = @order.id
-    redirect_to cart_path
+    redirect_to root_path
   end
 
   def destroy
